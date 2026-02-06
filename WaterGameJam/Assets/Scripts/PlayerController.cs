ef327f26ad062b5d53c75fad63d30429779bbee5
Toxin4426 <Toxin4426@gmail.com>
2026-02-06 12:02:38 -0500

    Player movement

--- WaterGameJam/Assets/Scripts/PlayerController.cs
+++ WaterGameJam/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Movement")]
+    private Vector3 movement;
+    [SerializeField] private int moveSpeed = 10;
+    public bool canMove = true;
+    public Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canMove ) return;
+        // Updates potion based on input
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
     public void OnMove(InputValue value)
     {
-        Vector2 move = value.Get<Vector2>();
-        Debug.Log(move);
+        movement = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
     }
 
     public void OnJump(InputValue value)
