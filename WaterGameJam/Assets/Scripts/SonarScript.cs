ebb9196ca9c59ddd8b1b4f9812592c1cc58433b5
PranavAlla <Pranavalla@gmail.com>
2026-02-07 12:14:19 -0500

    Created a prefab to hold the sonar pings and follow the player

--- WaterGameJam/Assets/Scripts/SonarScript.cs
+++ WaterGameJam/Assets/Scripts/SonarScript.cs
@@ -3,31 +3,16 @@ using UnityEngine.InputSystem;
 
 public class SonarScript : MonoBehaviour
 {
-    private float spreadAngle = 30f;
-    private float maxDistance = 20f;
-    private int numberOfRays = 30;
-    public GameObject pingPrefab;
+    public Transform playerTransform;
+    private float waterHeight = 0f;
 
     void Update()
     {
-        if (Keyboard.current.fKey.wasPressedThisFrame)
-        {
-            EmitSonar();
-            Debug.Log("Sonar Emitted");
-        }
+        FollowPlayer();
     }
-
-    private void EmitSonar()
+    private void FollowPlayer()
     {
-        for (int i = 0; i < numberOfRays; i++)
-        {
-            Vector3 direction = Quaternion.Euler(Random.Range(-spreadAngle, spreadAngle), Random.Range(-spreadAngle,spreadAngle), 0) * transform.forward;
-
-            if (Physics.Raycast(transform.position, direction, out RaycastHit hit, maxDistance))
-            {
-                Instantiate(pingPrefab, hit.point, hit.normal == Vector3.up ? Quaternion.identity : Quaternion.LookRotation(hit.normal));
-                Debug.Log(hit.normal);
-            }
-        }
+        Vector3 playerPosition = playerTransform.position;
+        this.transform.position = new Vector3(playerPosition.x, waterHeight, this.transform.position.z);
     }
 }
