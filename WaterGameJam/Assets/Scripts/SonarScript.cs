0c9c51a9826c32d335eca96d64b5c95b80ba2c18
PranavAlla <Pranavalla@gmail.com>
2026-02-07 16:21:35 -0500

    Saving sonar changes

--- WaterGameJam/Assets/Scripts/SonarScript.cs
+++ WaterGameJam/Assets/Scripts/SonarScript.cs
@@ -9,15 +9,16 @@ public class SonarScript : MonoBehaviour
     public GameObject player;
     private Transform playerTransform;
     private PlayerController playerController;
-    private float waterHeight = 0f;
+    private float waterHeight = 9f;
 
     [Header("Ping Settings")]
-    [SerializeField] private float underwaterPingsSpeed = 2.5f;
-    [SerializeField] private float abovewaterPingsSpeed = 5f;
+    private float underwaterPingsSpeed = 30f;
+    private float abovewaterPingsSpeed = 60f;
     private List<GameObject> underwaterPings = new List<GameObject>();
     private List<GameObject> abovewaterPings = new List<GameObject>();
     [SerializeField] private GameObject underwaterPingPrefab;
     [SerializeField] private GameObject abovewaterPingPrefab;
+    private float pingSizeDeathThreshold = 100f;
 
     void Start()
     {
@@ -34,21 +35,27 @@ public class SonarScript : MonoBehaviour
             SonarPing();
         }
 
+        DestroyPings();
+    }
+
+    void FixedUpdate()
+    {
         if (underwaterPings.Count > 0 || abovewaterPings.Count > 0)
         {
             MovePings();
         }
     }
+
     private void FollowPlayer()
     {
         Vector3 playerPosition = playerTransform.position;
-        this.transform.position = new Vector3(playerPosition.x, waterHeight, this.transform.position.z);
+        this.transform.position = new Vector3(playerPosition.x, waterHeight, playerPosition.z);
     }
 
     private void SonarPing()
     {
-        abovewaterPings.Add(Instantiate(abovewaterPingPrefab, this.transform.position += new Vector3(0, 10, 0), Quaternion.identity));
-        underwaterPings.Add(Instantiate(underwaterPingPrefab, this.transform.position += new Vector3(0, 10, 0), Quaternion.identity));
+        abovewaterPings.Add(Instantiate(abovewaterPingPrefab, this.transform.position += new Vector3(0, 20, 0), Quaternion.identity));
+        underwaterPings.Add(Instantiate(underwaterPingPrefab, this.transform.position -= new Vector3(0, 40, 0), Quaternion.identity));
         playerController.sonarEffect = false;
     }
 
@@ -77,6 +84,27 @@ public class SonarScript : MonoBehaviour
         catch
         {
             Debug.Log("Tried to move abovewater pings, but list was likely empty");
+        }
+    }
+
+    private void DestroyPings()
+    {
+        for (int i = underwaterPings.Count - 1; i >= 0; i--)
+        {
+            if (underwaterPings[i].transform.localScale.x >= pingSizeDeathThreshold)
+            {
+                Destroy(underwaterPings[i]);
+                underwaterPings.RemoveAt(i);
+            }
+        }
+
+        for (int i = abovewaterPings.Count - 1; i >= 0; i--)
+        {
+            if (abovewaterPings[i].transform.localScale.x >= pingSizeDeathThreshold)
+            {
+                Destroy(abovewaterPings[i]);
+                abovewaterPings.RemoveAt(i);
+            }
         }
     }
 }
