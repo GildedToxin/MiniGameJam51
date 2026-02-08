598e7daf7b44785077fd3ff768bbabb72962f473
PranavAlla <Pranavalla@gmail.com>
2026-02-07 19:23:49 -0500

    Saving work on sonarscript setup for walk and sneak pings

--- WaterGameJam/Assets/Scripts/SonarScript.cs
+++ WaterGameJam/Assets/Scripts/SonarScript.cs
@@ -14,11 +14,20 @@ public class SonarScript : MonoBehaviour
     [Header("Ping Settings")]
     private float underwaterPingsSpeed = 5f;
     private float abovewaterPingsSpeed = 20f;
+    private List<GameObject> underwaterWalkPings = new List<GameObject>();
+    private List<GameObject> abovewaterWalkPings = new List<GameObject>();
+    private List<GameObject> underwaterSneakPings = new List<GameObject>();
+    private List<GameObject> abovewaterSneakPings = new List<GameObject>();
     private List<GameObject> underwaterPings = new List<GameObject>();
     private List<GameObject> abovewaterPings = new List<GameObject>();
+
     [SerializeField] private GameObject underwaterPingPrefab;
     [SerializeField] private GameObject abovewaterPingPrefab;
-    private float pingSizeDeathThreshold = 100f;
+    private float pingSizeDeathThreshold = 30f;
+    private float pingSizeDeathThresholdSneaking = 5f;
+    private float pingSizeDeathThresholdWalking = 10f;
+
+    private bool playerIsSneaking = false;
 
     void Start()
     {
@@ -104,6 +113,104 @@ public class SonarScript : MonoBehaviour
             {
                 Destroy(abovewaterPings[i]);
                 abovewaterPings.RemoveAt(i);
+            }
+        }
+    }
+
+    private void MoveSneakingPings()
+    {
+        try
+        {
+            foreach (GameObject ping in underwaterSneakPings)
+            {
+                // increase scale of underwater pings
+                ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * underwaterPingsSpeed;
+            }
+        }
+        catch
+        {
+            Debug.Log("Tried to move underwater sneak pings, but list was likely empty");
+        }
+        
+        try
+        {
+            foreach (GameObject ping in abovewaterSneakPings)
+            {
+                ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * abovewaterPingsSpeed;
+            }
+        }
+        catch
+        {
+            Debug.Log("Tried to move abovewater sneak pings, but list was likely empty");
+        }
+    }
+
+    private void DestroySneakingPings()
+    {
+        for (int i = underwaterSneakPings.Count - 1; i >= 0; i--)
+        {
+            if (underwaterSneakPings[i].transform.localScale.x >= pingSizeDeathThresholdSneaking)
+            {
+                Destroy(underwaterSneakPings[i]);
+                underwaterSneakPings.RemoveAt(i);
+            }
+        }
+
+        for (int i = abovewaterSneakPings.Count - 1; i >= 0; i--)
+        {
+            if (abovewaterSneakPings[i].transform.localScale.x >= pingSizeDeathThresholdSneaking)
+            {
+                Destroy(abovewaterSneakPings[i]);
+                abovewaterSneakPings.RemoveAt(i);
+            }
+        }
+    }
+
+    private void MoveWalkingPings()
+    {
+        try
+        {
+            foreach (GameObject ping in underwaterWalkPings)
+            {
+                // increase scale of underwater pings
+                ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * underwaterPingsSpeed;
+            }
+        }
+        catch
+        {
+            Debug.Log("Tried to move underwater sneak pings, but list was likely empty");
+        }
+        
+        try
+        {
+            foreach (GameObject ping in abovewaterWalkPings)
+            {
+                ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * abovewaterPingsSpeed;
+            }
+        }
+        catch
+        {
+            Debug.Log("Tried to move abovewater sneak pings, but list was likely empty");
+        }
+    }
+
+    private void DestroyWalkingPings()
+    {
+        for (int i = underwaterWalkPings.Count - 1; i >= 0; i--)
+        {
+            if (underwaterSneakPings[i].transform.localScale.x >= pingSizeDeathThresholdWalking)
+            {
+                Destroy(underwaterWalkPings[i]);
+                underwaterWalkPings.RemoveAt(i);
+            }
+        }
+
+        for (int i = abovewaterWalkPings.Count - 1; i >= 0; i--)
+        {
+            if (abovewaterSneakPings[i].transform.localScale.x >= pingSizeDeathThresholdWalking)
+            {
+                Destroy(abovewaterWalkPings[i]);
+                abovewaterWalkPings.RemoveAt(i);
             }
         }
     }
