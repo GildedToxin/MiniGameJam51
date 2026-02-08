1aab06da97921874c5c07b39bc97dda1cb2e351c
PranavAlla <Pranavalla@gmail.com>
2026-02-08 03:56:20 -0500

    auto enemy ping for testing

--- WaterGameJam/Assets/Scripts/AISonarScript.cs
+++ WaterGameJam/Assets/Scripts/AISonarScript.cs
@@ -5,7 +5,17 @@ public class AISonarScript : MonoBehaviour
 {
     private float waterHeight = 9f;
 
-    private List<GameObject> aiPings = new List<GameObject>();
+    private float underwaterPingsSpeed = 10f;
+    private float abovewaterPingsSpeed = 15f;
+    private float pingSizeDeathThreshold = 15f;
+
+    private List<GameObject> aiPingsAboveWater = new List<GameObject>();
+    private List<GameObject> aiPingsUnderWater = new List<GameObject>();
+    [SerializeField] private GameObject aiPingPrefabAboveWater;
+    [SerializeField] private GameObject aiPingPrefabUnderWater;
+
+    private float autoPingCooldown = 10f;
+    private float autoPingTimer = 0f;
 
     void Start()
     {
@@ -14,6 +24,68 @@ public class AISonarScript : MonoBehaviour
 
     void Update()
     {
+        MovePings();
+        TimerPing();
+        DestroyPings();
+    }
+
+    private void MovePings()
+    {
+        try
+        {
+            foreach (GameObject ping in aiPingsUnderWater)
+            {
+                // increase scale of underwater pings
+                ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * underwaterPingsSpeed;
+            }
+        }
+        catch
+        {
+            Debug.Log("Tried to move underwater pings, but list was likely empty");
+        }
         
+        try
+        {
+            foreach (GameObject ping in aiPingsAboveWater)
+        {
+            ping.transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime * abovewaterPingsSpeed;
+        }
+        }
+        catch
+        {
+            Debug.Log("Tried to move abovewater pings, but list was likely empty");
+        }
+    }
+
+    private void DestroyPings()
+    {
+        for (int i = aiPingsUnderWater.Count - 1; i >= 0; i--)
+        {
+            if (aiPingsUnderWater[i].transform.localScale.x >= pingSizeDeathThreshold)
+            {
+                Destroy(aiPingsUnderWater[i]);
+                aiPingsUnderWater.RemoveAt(i);
+            }
+        }
+
+        for (int i = aiPingsAboveWater.Count - 1; i >= 0; i--)
+        {
+            if (aiPingsAboveWater[i].transform.localScale.x >= pingSizeDeathThreshold)
+            {
+                Destroy(aiPingsAboveWater[i]);
+                aiPingsAboveWater.RemoveAt(i);
+            }
+        }
+    }
+
+    private void TimerPing()
+    {
+        autoPingTimer -= Time.deltaTime;
+        if (autoPingTimer <= 0)
+        {
+            aiPingsAboveWater.Add(Instantiate(aiPingPrefabAboveWater, this.transform.position += new Vector3(0, 25.25f + waterHeight - this.transform.position.y, 0), Quaternion.identity));
+            aiPingsUnderWater.Add(Instantiate(aiPingPrefabUnderWater, this.transform.position -= new Vector3(0, 16.25f + this.transform.position.y, 0), Quaternion.identity));
+            autoPingTimer = autoPingCooldown;
+        }
     }
 }
