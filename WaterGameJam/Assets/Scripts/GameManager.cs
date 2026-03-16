88c661135a7a03b1fcb4ea52dea32204030a7386
Toxin4426 <Toxin4426@gmail.com>
2026-03-16 01:05:48 -0400

    fix

--- WaterGameJam/Assets/Scripts/GameManager.cs
+++ WaterGameJam/Assets/Scripts/GameManager.cs
@@ -48,7 +48,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            if(SceneManager.GetActiveScene().name != "MainMenu")
+            if(SceneManager.GetActiveScene().name == "MainMenu")
             {
                 return;
             }
